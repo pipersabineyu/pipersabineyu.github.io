3481a235eca3aef22c5dd5f66d567ea169bf5e91
Piper Yu <pyu@Pipers-MacBook-Pro.local>
2026-08-06 23:57:50 -0700

    Remove the "More" section from the homepage
    Dropped the "Read more about me... or get in touch at..." block
    entirely - the page now ends right after the last project. Removed
    the now-unused MoreSection component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { HomeExperience } from "@/components/home/HomeExperience";
-import { MoreSection } from "@/components/home/MoreSection";
 
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col">
       <HomeExperience />
-      <MoreSection />
     </div>
   );
 }
